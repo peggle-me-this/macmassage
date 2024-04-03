22848a6c6684c60bb9bb693375a28358756ee13c
Bronwyn Barnes <bronwyn.d.barnes@gmail.com>
2024-04-03 11:16:22 +0200

    Made 3 different attemps at ./make html

---
+++ docs/build/html/_sources/index.rst.txt
@@ -0,0 +1,20 @@
+.. Macmassage Website documentation master file, created by
+   sphinx-quickstart on Wed Apr  3 11:10:48 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Macmassage Website's documentation!
+==============================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,34 +10,27 @@ import sys
 import django
 
 sys.path.insert(0, os.path.abspath('..'))
-os.environ['DJANGO_SETTINGS_MODULE'] = 'macmassage_site.settings'
-#django.setup()
 
-project = 'docs'
-copyright = '2024, Bronwyn Barnes'
-author = 'Bronwyn Barnes'
-release = 'development'
+# Set the Django settings module environment variable for the specific app
+os.environ['DJANGO_SETTINGS_MODULE'] = 'macmassage_site.settings'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'sphinx_rtd_theme',
+    'sphinx.ext.napoleon',  # Google-style docstrings
+    'sphinx.ext.viewcode',
+    'sphinx.ext.todo',
+    'sphinx.ext.githubpages',
 ]
 
-
 templates_path = ['_templates']
-exclude_patterns = ['templates']
-
-
-
+exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-
 html_static_path = ['_static']
 
